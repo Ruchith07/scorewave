5285e4174f21b1fae20fdc2c8d25364fd5e8b9ef
Ruchith07 <98446189+Ruchith07@users.noreply.github.com>
2023-08-22 19:37:42 +0530

    changes made in css so that website can be viewed in mobile properly

--- scorewave/src/App.jsx
+++ scorewave/src/App.jsx
@@ -12,19 +12,23 @@ function App() {
         <div>Pakistan</div>
       </div>  
       <div className="upper">
-        <div className="inner-upper"></div>
+        <div className="divA">Team A</div>
+        <div className="divB">Team B</div>
       </div>
-      <div className="SummaryAndScorecardContainer">
-            <div>SUMMARY</div>
-            <div>SCORECARD</div>
+      <div className="hello">
+            <div className="SummaryAndScorecardContainer">
+              <div>SUMMARY</div>
+              <div>SCORECARD</div>
+            </div>
       </div>
       <div className="Playerslist">
         <div className="sctitles">
-          <div>R</div>
-          <div>B</div>
-          <div>4's</div>
-          <div>6's</div>
-          <div>S.R</div>
+          <div>PLAYER NAME</div>
+          <div className="values">R</div>
+          <div className="values">B</div>
+          <div className="values">4's</div>
+          <div className="values">6's</div>
+          <div className="values">S.R</div>
         </div>
                   <div>Sehwag</div>
                   <div>Ghambir</div>
